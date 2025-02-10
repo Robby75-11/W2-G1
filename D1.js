0815a8566df7f67319d4bb8acd978bdf9f72ac9b
Roberto Albergo <ralbergo7@gmail.com>
2025-02-10 16:56:00 +0100

    robby75
    risultati

---
+++ D1.js
@@ -0,0 +1,57 @@
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/ String, NUMBER, BOOLEANO;
+console.log("NUMBER indica numeri");
+console.log("String indica caratteri testi");
+console.log("booleano indica valori vero o falso");
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+ myname = "Roberto"
+myname=Roberto
+console.log (my name);
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+addition = addition + number;
+let n1 = 12;
+let n2 = 20;
+console.log(n1 + n2);
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+let x = 12;
+/*   */
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+const myname = Roberto;
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+let number1 = 4;
+let x = 12;
+console.Log(x - number1);
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+let name1 = "john";
+let name2 = "John";
+console.log(name1 == name2);
+/* SCRIVI QUI LA TUA RISPOSTA */
